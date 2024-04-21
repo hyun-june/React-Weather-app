7ad4e13f9bfc5797a9557f1dbcfdb99982392079
Hyunjun Lee <leehyunjune11@gmail.com>
2024-04-21 18:50:56 +0900

    day1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 
+// 1. 앱이 실행되자마자 현재 위치 기반의 날씨가 보인다.
+// 2. 날씨 정보에는 도시, 섭씨, 화씨, 날씨 상태
+// 3. 5개의 버튼이 있다(1개는 현재위치, 4개는 다른도시)
+// 4. 도시 버튼을 클릭할때 마다 도시별 날씨가 나온다.
+// 5. 현재위치 버튼을 누르면 다시 현재 위치 기반의 날씨가 나온다.
+// 6. 데이터를 들고오는 동안 로딩 스피너가 돈다.
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      
     </div>
   );
 }
