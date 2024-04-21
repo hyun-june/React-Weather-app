d41cd1ded88014ba00323daef8885810b0c1190c
Hyunjun Lee <leehyunjune11@gmail.com>
2024-04-21 19:51:51 +0900

    day1 API

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
+import {useEffect, useState} from "react"
 import './App.css';
+
+const API_KEY = '040347aad8eefbe8027a0cb58edd9c93';
 
 // 1. 앱이 실행되자마자 현재 위치 기반의 날씨가 보인다.
 // 2. 날씨 정보에는 도시, 섭씨, 화씨, 날씨 상태
@@ -7,6 +10,27 @@ import './App.css';
 // 5. 현재위치 버튼을 누르면 다시 현재 위치 기반의 날씨가 나온다.
 // 6. 데이터를 들고오는 동안 로딩 스피너가 돈다.
 function App() {
+  const getCurrentLocation=()=>{
+    navigator.geolocation.getCurrentPosition((position)=>{
+      let lat = position.coords.latitude
+      let lon = position.coords.longitude
+      console.log("현재",lat,lon)
+      getWeatherData(lat,lon);
+      console.log(getWeatherData())
+    });
+  }
+
+  const getWeatherData = async(lat,lon)=>{
+    let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data)
+  }
+
+  useEffect(()=>{
+    getCurrentLocation()
+  },[])
+
   return (
     <div>
       
